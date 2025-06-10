ddc600159f2de9f0be308c51a862fab245a3c1a1
tomasKarecka <tmzkarecka@gmail.com>
2025-06-10 11:40:06 +0300

    Initial commit

---
+++ src/main/java/lt/tomas/vehicle_app_backend/VehicleAppBackendApplication.java
@@ -0,0 +1,13 @@
+package lt.tomas.vehicle_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleAppBackendApplication.class, args);
+	}
+
+}
